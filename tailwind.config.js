98bc18fdd11741d0efa728258d067e30cf7464ec
baby-sal <sally.facebook123@gmail.com>
2025-05-26 18:14:12 +0100

    Tailwind connected and files in the right places

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    content: ["./src/**/*.html"], // or your specific paths
+    content: [
+        "./main/home.html"
+    ],
     theme: {
         extend: {
             fontFamily: {
