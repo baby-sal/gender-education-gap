fed655c3aa8cf96acd98e1dea3489b52da35133c
baby-sal <sally.facebook123@gmail.com>
2025-06-05 19:16:31 +0100

    Working multiple pages with react router

--- mind-the-gap/src/App.js
+++ mind-the-gap/src/App.js
@@ -1,14 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello world</p>
-      </header>
-    </div>
-  );
-}
+import { NavLink, Routes, Route } from 'react-router-dom';
+
+const App = () => (
+  <div className='app'>
+    <h1>Mind the gap</h1>
+    <Navigation />
+    <Main />
+  </div>
+);
 
 export default App;
+
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li><NavLink to='/'>Home</NavLink></li>
+      <li><NavLink to='/about'>About</NavLink></li>
+      <li><NavLink to='/contact'>Data</NavLink></li>
+    </ul>
+  </nav>
+);
+
+const Home = () => (
+  <div className='home'>
+    <h1>Welcome to my portfolio website</h1>
+    <p> Feel free to browse around and learn more about me.</p>
+  </div>
+);
+
+const About = () => (
+  <div className='about'>
+    <h1>About Me</h1>
+    <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+    <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+  </div>
+);
+
+const Contact = () => (
+  <div className='contact'>
+    <h1>Contact Me</h1>
+    <p>You can reach me via email: <strong>hello@example.com</strong></p>
+  </div>
+);
+
+
+
+const Main = () => (
+  <Routes>
+    <Route path='/' element={<Home />}></Route>
+    <Route path='/about' element={<About />}></Route>
+    <Route path='/contact' element={<Contact />}></Route>
+  </Routes>
+);
